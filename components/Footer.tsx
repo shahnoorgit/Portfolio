b68b58605f848aaf7b597acb206409b90914471e
shahnoorgit <shahnoormujawar@gmail.com>
2024-06-22 21:17:22 +0530

    linting bug fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,27 +6,27 @@ import { socialMedia } from "@/Data";
 const Footer = () => {
   return (
     <footer id="contact" className="w-full mb-[100px] md:mb-[5px] pb-10">
-      <div className=" flex flex-col items-center">
-        <h1 className=" heading lg:w-[65vw]">
-          Made your <span className=" text-purple">Decision?</span>
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:w-[65vw]">
+          Made your <span className="text-purple">Decision?</span>
         </h1>
-        <p className=" text-lg font-semibold text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let's discuss how can i contribute to your
-          company's success.
+        <p className="text-lg font-semibold text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today and let&apos;s discuss how can I contribute to
+          your company&apos;s success.
         </p>
         <a href="mailto:shahnoormujawar@gmail.com">
           <MagicButton
-            title=" Let's get in touch"
+            title="Let's get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
         </a>
       </div>
-      <div className=" flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className=" md:text-base text-sm md:font-normal font-light">
-          copyright © 2024 Shahnoor{" "}
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          copyright &copy; 2024 Shahnoor
         </p>
-        <div className=" flex items-center md:gap-3 gap-6">
+        <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((social) => (
             <div
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 backdrop-filter rounded-lg border border-black-300"
