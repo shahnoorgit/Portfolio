1baa6ae5827c5f7799c0aeb4331ab5c70da7e9f4
shahnoorgit <shahnoormujawar@gmail.com>
2024-06-24 20:18:20 +0530

    copy mail added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -54,6 +54,11 @@ export const BentoGridItem = ({
   const rightLists = ["Tailwindcss", "Typescript", "ExpressJS"];
 
   const [copied, setCopied] = useState(false);
+  const handleCopy = () => {
+    const text = "shahnoormujawar@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
 
   const defaultOptions = {
     loop: copied,
@@ -141,6 +146,30 @@ export const BentoGridItem = ({
                   </span>
                 ))}
               </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+              {/* button border magic from tailwind css buttons  */}
+              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
+              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+              {/* add handleCopy() for the copy the text */}
+              <div
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
+              >
+                {/* <img src="/confetti.gif" alt="confetti" /> */}
+                <Lottie options={defaultOptions} height={200} width={400} />
+              </div>
+
+              <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
             </div>
           )}
         </div>
